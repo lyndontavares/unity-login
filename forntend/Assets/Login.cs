ca2bb32270a545fd724dea536f7dda58d6355aa8
lyndontavares <lyndon.tavares@datapar.com.py>
2021-03-27 09:51:08 -0300

    ok

--- forntend/Assets/Login.cs
+++ forntend/Assets/Login.cs
@@ -7,37 +7,36 @@ using UnityEngine.Networking;
 public class Login : MonoBehaviour
 {
     public void DoLogin() {
-
         Debug.Log("Iniciando login...");
-
-         StartCoroutine(callLoginTeste());
-        // A correct website page.
-        //StartCoroutine(GetRequest("https://www.example.com"));
-        // A non-existing page.
-        //StartCoroutine(GetRequest("https://error.html"));
+        StartCoroutine(callLoginTeste());
     }
 
     public IEnumerator callLoginTeste()
     {
         Debug.Log("Logando...");
 
+        // Preparando JSON para o request
         string bodyJsonString = "{\"username\":\"isa\",\"password\":\"123456\"}";
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(bodyJsonString);
 
         UnityWebRequest www = new UnityWebRequest("http://localhost:8080/api/auth/signin", "POST");
         www.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
         www.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-
         www.SetRequestHeader("Content-Type", "application/json");
+
         yield return www.SendWebRequest();
         if (www.error != null) {
             Debug.Log("Error: "+www.error);
         } else {
             Debug.Log("Response: "+www.downloadHandler.text);
         }
-
     }
 
+
+    // A correct website page.
+    //StartCoroutine(GetRequest("https://www.example.com"));
+    // A non-existing page.
+    //StartCoroutine(GetRequest("https://error.html"));
 
     IEnumerator GetRequest(string uri)
     {
